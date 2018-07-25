b879a8b2dc4ad618789fee314455e238476c9a93
Becky White <trns4md@charter.net>
2018-07-24 22:16:06 -0400

    background added

---
+++ app.js
@@ -0,0 +1,29 @@
+const catLineup = [{
+        name: 'Calico Kitty',
+        image: 'images/calico.jpg'
+    },
+    {
+        name: 'Charlie Kitty',
+        image: 'images/charlie.jpg'
+    },
+    {
+        name: 'Family of Tabbies',
+        image: 'images/kittyLitter.jpg'
+    },
+    {
+        name: 'Tuxedo Kitty',
+        image: 'images/mugsy.jpg'
+    },
+    {
+        name: 'Orange Tabby',
+        image: 'images/orangeTabby.jpg'
+    },
+    {
+        name: 'Enemy divine',
+        image: 'images/puppyLove'
+    },
+    {
+        name: 'Kitty on the half shell',
+        image: 'images/violet.JPG'
+    }
+];
