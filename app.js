2056dd4f74345114ddf55e9a2ba61d016a1ea2ed
Becky White <trns4md@charter.net>
2018-08-07 16:31:55 -0400

    Added cat name list

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-const kittyLitter = "Kitty Litter";
-const charlie = "Charlie Cat";
+let display = document.getElementsByClassName("displayCat");
+let nameList = document.getElementsByClassName("catName");
+let catPost = document.getElementsByClassName("catPic");
+let counter = document.getElementsByClassName("clickCount");
+
+
+const rollCall = [{
+        name: 'Charlie',
+        image: 'images/charlie.jpg',
+    },
+    {
+        name: 'Mugsy',
+        image: 'images/mugsy.jpg',
+    },
+    {
+        name: 'Pumpkin',
+        image: 'images/orangeTabby.jpg',
+    },
+    {
+        name: 'Scooby Gang',
+        image: 'images/kittyLitter.jpg',
+    },
+    {
+        name: 'Violet',
+        image: 'images/violet.jpg',
+    }
+];
+
+
+for (i = 0; i < rollCall.length; i++) {
+    let name = rollCall[i].name;
+    let image = rollCall[i].image;
+    let moves = 0;
+    let li = document.createElement('li');
+    let node = document.createTextNode(name);
+    li.appendChild(node);
+
+
+    let ul = document.getElementById('catList');
+    ul.appendChild(li);
 
 
 
-let catPicture = document.getElementById('kittyClowder');
-let moves = 0;
-catPicture.addEventListener('click', function() {
-    let click = document.getElementById('catCounter');
 
-    moves++;
-    click.innerHTML = "Clicks:" + moves;
-});
-
-let nextPicture = document.getElementById('charlieCat');
-let choose = 0;
-nextPicture.addEventListener('click', function() {
-    let click = document.getElementById('nextCounter');
-
-    choose++;
-    click.innerHTML = "Clicks:" + choose;
-});
+    // li.addEventListener('click', function(name, image, moves) {
+    //moves++;
+    // click.innerHTML = "Clicks:" + moves;
+};
